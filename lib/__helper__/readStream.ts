209bd61a59902ce4fec6dce6a926a972a95ae9c2
Nick K <nick.kruchinin@gmail.com>
2021-05-11 03:24:18 +0300

    Add readStream helper

---
+++ lib/__helper__/readStream.ts
@@ -0,0 +1,45 @@
+import {Readable} from "stream"
+
+type Input = Readable | {
+  [Symbol.asyncIterator](): AsyncIterableIterator<any>
+}
+
+async function readToBuffer(input: Input): Promise<Buffer> {
+  const chunks = []
+
+  for await (const chunk of input) {
+    chunks.push(chunk)
+  }
+
+  return Buffer.concat(chunks)
+}
+
+async function readToString(input: Input): Promise<string> {
+  let string = ""
+
+  const decoder = new TextDecoder()
+
+  for await (const chunk of input) {
+    string += decoder.decode(chunk, {stream: true})
+  }
+
+  string += decoder.decode()
+
+  return string
+}
+
+async function readStream(input: Input): Promise<Buffer>
+async function readStream(
+  input: Input,
+
+  toString: boolean
+): Promise<string>
+async function readStream(
+  input: Input,
+
+  toString: boolean = false
+): Promise<string | Buffer> {
+  return toString ? readToString(input) : readToBuffer(input)
+}
+
+export default readStream
