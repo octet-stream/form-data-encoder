887e19d91dc64ed0be694eddba8bc22eaee6dd70
Nick K <nick.kruchinin@gmail.com>
2021-05-15 21:17:27 +0300

    Replace crypto with nanoid for default boundary generator.

--- lib/util/createBoundary.ts
+++ lib/util/createBoundary.ts
@@ -1,10 +1,11 @@
-import {randomBytes} from "crypto"
+import {customAlphabet} from "nanoid"
+
+const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
+const generate = customAlphabet(alphabet, 16)
 
 /**
  * Generates a boundary string for FormData encoder.
  */
-const createBoundary = (): string => (
-  `form-data-boundary-${randomBytes(16).toString("hex")}`
-)
+const createBoundary = (): string => `form-data-boundary-${generate()}`
 
 export default createBoundary
