e0e3801eb14f7daf8e371e0861d4f4617fcaa7fa
Nick K <nick.kruchinin@gmail.com>
2021-05-26 19:27:38 +0300

    Expose isFile and isFormData helpers.

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,6 @@
 export * from "./FormDataLike"
 export * from "./FileLike"
 export * from "./Encoder"
+
+export {default as isFile} from "./util/isFile"
+export {default as isFormData} from "./util/isFormData"
