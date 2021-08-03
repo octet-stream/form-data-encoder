e209e06cf2ba50d69eb775367a4baaa252a55d61
Nick K <nick.kruchinin@gmail.com>
2021-08-03 22:57:33 +0300

    Slightly optimized createBoundary helper and separated config for .d.ts output.

--- lib/util/createBoundary.ts
+++ lib/util/createBoundary.ts
@@ -10,7 +10,14 @@ function createBoundary(size: number): string {
   let res = ""
 
   while (size--) {
-    res += alphabet[Math.floor(Math.random() * alphabet.length)]
+    // I use bitwise `<<` here for slightly more performant string fill.
+    // It will basically do same thing as `Math.truc()`,
+    // except it only support signed 32-bit integers.
+    // Because the result of this operation will always be
+    // a number in range `0` and `alphabet.length - 1` (inclusive),
+    // so we don't need `Math.floor()` too.
+    /* eslint no-bitwise: ["error", {"allow": ["<<"]}] */
+    res += alphabet[(Math.random() * alphabet.length) << 0]
   }
 
   return res
