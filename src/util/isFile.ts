4ffe3c48efc2a6b93916c22996baebfde6323758
Nick K <nick.kruchinin@gmail.com>
2022-05-21 23:01:12 +0300

    Remove documentation for deprecated isFileLike helper

--- src/util/isFile.ts
+++ src/util/isFile.ts
@@ -42,33 +42,6 @@ export const isFile = (value: unknown): value is FileLike => Boolean(
 )
 
 /**
- * Check if given object is `File`.
- *
- * Note that this function will return `false` for Blob, because the FormDataEncoder expects FormData to return File when a value is binary data.
- *
- * @param value an object to test
- *
- * @api public
- *
  * @deprecated use `isFile` instead
- *
- * This function will return `true` for FileAPI compatible `File` objects:
- *
- * ```
- * import {isFileLike} from "form-data-encoder"
- *
- * isFileLike(new File(["Content"], "file.txt")) // -> true
- * ```
- *
- * However, if you pass a Node.js `Buffer` or `ReadStream`, it will return `false`:
- *
- * ```js
- * import {createReadStream} from "node:fs"
- *
- * import {isFileLike} from "form-data-encoder"
- *
- * isFileLike(Buffer.from("Content")) // -> false
- * isFileLike(createReadStream("path/to/a/file.txt")) // -> false
- * ```
- */
+  */
 export const isFileLike = isFile
