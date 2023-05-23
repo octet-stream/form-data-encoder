d92c27b60d55b01f95e34e7f906abb81f5b203d7
Nick K <nick.kruchinin@gmail.com>
2023-05-23 20:04:47 +0300

    Fixes for chunk utility documentation

--- src/util/chunk.ts
+++ src/util/chunk.ts
@@ -2,11 +2,11 @@ export const MAX_CHUNK_SIZE = 65536
 
 /**
  * Chunks given `value` into evenly sized pieces (Same as `MAX_CHUNK_SIZE` bytes per each).
- * Returns `Generator<Uint8Array>` allowing to iterate over these chunks.
+ * Returns `Generator<Uint8Array, void>` allowing to iterate over these chunks.
  *
- * If value is less then `MAX_CHUNK_SIZE`, it will be returned as-is.
+ * If the `value` <= `MAX_CHUNK_SIZE`, it will be returned as-is.
  *
- * If the last chunk is less than `MAX_CHUNK_SIZE`, then returned value will be the size that chunk.
+ * If the last chunk's size is less than `MAX_CHUNK_SIZE`, then returned value will be the size that chunk.
  *
  * @param value A value to chunk into evenly sized pieces
  *
